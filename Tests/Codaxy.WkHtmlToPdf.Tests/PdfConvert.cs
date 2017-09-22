b4462d399ff6e979c48ce734d1a43bcb707aa6f4
ChrisGarber-Medlever <christopher.garber@medlever.com>
2017-09-22 14:48:28 -0400

    Converted from string to String

--- Tests/Codaxy.WkHtmlToPdf.Tests/PdfConvert.cs
+++ Tests/Codaxy.WkHtmlToPdf.Tests/PdfConvert.cs
@@ -11,7 +11,7 @@ namespace Codaxy.WkHtmlToPdf
 {
    public static class PaperTypes
    {
-      public static readonly List<string> ValidSizes = new List<string> {
+      public static readonly List<String> ValidSizes = new List<String> {
             "A0",
             "A1",
             "A2",
@@ -44,38 +44,38 @@ namespace Codaxy.WkHtmlToPdf
             "Tabloid"
       };
 
-      public static string A0 = "A0";
-      public static string A1 = "A1";
-      public static string A2 = "A2";
-      public static string A3 = "A3";
-      public static string A4 = "A4";
-      public static string A5 = "A5";
-      public static string A6 = "A6";
-      public static string A7 = "A7";
-      public static string A8 = "A8";
-      public static string A9 = "A9";
-      public static string B0 = "B0";
-      public static string B1 = "B1";
-      public static string B10 = "B10";
-      public static string B2 = "B2";
-      public static string B3 = "B3";
-      public static string B4 = "B4";
-      public static string B5 = "B5";
-      public static string B6 = "B6";
-      public static string B7 = "B7";
-      public static string B8 = "B8";
-      public static string B9 = "B9";
-      public static string C5E = "C5E";
-      public static string Comm10E = "Comm10E";
-      public static string DLE = "DLE";
-      public static string Executive = "Executive";
-      public static string Folio = "Folio";
-      public static string Ledger = "Ledger";
-      public static string Legal = "Legal";
-      public static string Letter = "Letter";
-      public static string Tabloid = "Tabloid";
+      public static String A0 = "A0";
+      public static String A1 = "A1";
+      public static String A2 = "A2";
+      public static String A3 = "A3";
+      public static String A4 = "A4";
+      public static String A5 = "A5";
+      public static String A6 = "A6";
+      public static String A7 = "A7";
+      public static String A8 = "A8";
+      public static String A9 = "A9";
+      public static String B0 = "B0";
+      public static String B1 = "B1";
+      public static String B10 = "B10";
+      public static String B2 = "B2";
+      public static String B3 = "B3";
+      public static String B4 = "B4";
+      public static String B5 = "B5";
+      public static String B6 = "B6";
+      public static String B7 = "B7";
+      public static String B8 = "B8";
+      public static String B9 = "B9";
+      public static String C5E = "C5E";
+      public static String Comm10E = "Comm10E";
+      public static String DLE = "DLE";
+      public static String Executive = "Executive";
+      public static String Folio = "Folio";
+      public static String Ledger = "Ledger";
+      public static String Legal = "Legal";
+      public static String Letter = "Letter";
+      public static String Tabloid = "Tabloid";
 
-      public static bool isValidPaperType(string test)
+      public static bool isValidPaperType(String test)
       {
          return ValidSizes.Contains(test);
       }
